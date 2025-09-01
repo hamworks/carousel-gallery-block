0974ff551506c125e4fba905280c8816a0888da5
chiilog <purgexpurge@gmail.com>
2025-09-01 17:34:47 +0900

    fix(Images): improve image rendering and key assignment
    - Update key assignment to use nullish coalescing for better handling of image IDs.
    - Refactor image rendering to conditionally display the <img> element only if the image URL is present, enhancing performance and preventing unnecessary DOM elements.

--- src/Images.tsx
+++ src/Images.tsx
@@ -8,15 +8,17 @@ const Images = ( { images }: Props ) => (
 	<div className="wp-block-hamworks-carousel-gallery-block__images">
 		{ images.map( ( image, index ) => (
 			<div
-				key={ image.id || `temp-${ index }` }
+				key={ image.id ?? `temp-${ index }` }
 				className="wp-block-hamworks-carousel-gallery-block__image"
 			>
-				<img
-					src={ image.url || '' }
-					data-id={ image.id || null }
-					alt={ image.alt || '' }
-					loading="lazy"
-				/>
+				{ image.url && (
+					<img
+						src={ image.url }
+						data-id={ image.id ?? null }
+						alt={ image.alt || '' }
+						loading="lazy"
+					/>
+				) }
 			</div>
 		) ) }
 	</div>
